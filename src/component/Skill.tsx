fcba3a3dd62bfbbb93555da2834b3e329a5a942f
JAYCEE <Jayceexxiii@gmail.com>
2023-05-17 21:50:26 -0700

    adjusting skill tailwaincss

--- src/component/Skill.tsx
+++ src/component/Skill.tsx
@@ -9,74 +9,69 @@ import mongodbLogo from "../img/logos/mongodb.svg";
 import reactLogo from "../img/logos/react.svg";
 import mysqlLogo from "../img/logos/mysql.svg";
 
-
 function skill() {
   return (
     <div>
-      <section id="skills" className="p-4 md:p-5">
-        <div className="container mx-auto px-5">
+      <section id="skills" className="p-6 bg-gray-50">
+        <div className="container mx-auto">
           <div className="text-center mb-10">
-            <h2 className="text-4xl font-bold">Skills</h2>
+            <h2 className="textthebold mb-4 text-3xl font-bold text-center text-gray-800">
+              Skills
+            </h2>
           </div>
         </div>
 
-        <div className="container mx-auto px-5">
-          <div
-            id="skills"
-            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-4 md:mt-5"
-          >
+        <div className="[]container mx-auto px-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {/* HTML Skill */}
-            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3">
+            <div className="shadow-md rounded-lg p-4 flex items-center space-x-4 bg-white">
               <div className="flex justify-center items-center">
-                <img
-                  src={htmlLogo}
-                  alt=""
-                  className="logo w-full h-auto"
-                />
+                <img src={htmlLogo} alt="HTML Logo" className="w-16 h-16" />
               </div>
               <div>
-                <h5 className="mb-0 font-medium text-gray-700">HTML</h5>
-                <p className="text-gray-500 mb-0">2 Years Experience</p>
+                <h5 className="mb-0 text-xl font-semibold text-gray-800">
+                  HTML
+                </h5>
+                <p className="text-gray-600">2 Years Experience</p>
               </div>
             </div>
 
             {/* CSS Skill */}
-            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3">
+            {/* CSS Skill */}
+            <div className="shadow-md rounded-lg p-4 flex items-center space-x-4 bg-white">
               <div className="flex justify-center items-center">
-                <img
-                  src={cssLogo}
-                  alt=""
-                  className="logo w-full h-auto"
-                />
+                <img src={cssLogo} alt="CSS Logo" className="w-16 h-16" />
               </div>
               <div>
-                <h5 className="mb-0 font-medium text-gray-700">CSS</h5>
-                <p className="text-gray-500 mb-0">2 Years Experience</p>
+                <h5 className="mb-0 text-xl font-semibold text-gray-800">
+                  CSS
+                </h5>
+                <p className="text-gray-600">2 Years Experience</p>
               </div>
             </div>
 
-            {/* JavaScript Skill */}
-            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3">
+ {/* JavaScript Skill */}
+ <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3 bg-white">
               <div className="flex justify-center items-center">
                 <img
                   src={jsLogo}
                   alt=""
-                  className="logo w-full h-auto"
+                  className="w-16 h-16"
                 />
               </div>
               <div>
-                <h5 className="mb-0 font-medium text-gray-700">JavaScript</h5>
+                <h5 className="mb-0 font-medium text-gray-700 ">JavaScript</h5>
                 <p className="text-gray-500 mb-0">1 Year Experience</p>
               </div>
             </div>
 
             {/* Java Skill */}
-            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3">
+            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3  bg-white">
               <div className="flex justify-center items-center">
                 <img
                   src={javaLogo}
                   alt=""
-                  className="logo w-full h-auto"
+                  className="w-16 h-16"
                 />
               </div>
               <div>
@@ -86,12 +81,12 @@ function skill() {
             </div>
 
             {/* Python Skill */}
-            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3">
+            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3  bg-white">
               <div className="flex justify-center items-center">
                 <img
                   src={jsLogo}
                   alt=""
-                  className="logo w-full h-auto"
+                  className="w-16 h-16"
                 />
               </div>
               <div>
@@ -100,59 +95,56 @@ function skill() {
               </div>
             </div>
 
-            <div className="col mb-3">
-              <div className="container">
-                <div className="row skill-item rounded p-2 p-md-3 shadow-sm d-flex align-items-center">
-                  <div className="col-3 thumb-md d-flex flex-wrap justify-content-center align-items-center">
+
+            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3  bg-white">
+              <div className="flex justify-center items-center">
                     <img
                       src={mongodbLogo}
                       alt=""
-                      className="logo img-fluid"
+                      className="w-16 h-16"
                     />
                   </div>
-                  <div className="col-9">
-                    <h5 className="mb-0 fw-medium text-gray-700">Mongodb</h5>
-                    <p className="exp text-muted mb-0">2 Years Experience</p>
-                  </div>
-                </div>
+                  <div>
+                    <h5 className="mb-0 font-medium text-gray-700">Mongodb</h5>
+                    <p className="text-gray-500 mb-0">2 Years Experience</p>
+                  
               </div>
             </div>
 
-            <div className="col mb-3">
-              <div className="container">
-                <div className="row skill-item rounded p-2 p-md-3 shadow-sm d-flex align-items-center">
-                  <div className="col-3 thumb-md d-flex flex-wrap justify-content-center align-items-center">
+            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3  bg-white">
+              <div className="flex justify-center items-center">
                     <img
                       src={reactLogo}
                       alt="c"
-                      className="logo img-fluid"
+                      className="w-16 h-16"
                     />
                   </div>
-                  <div className="col-9">
-                    <h5 className="mb-0 fw-medium text-gray-700">React</h5>
-                    <p className="exp text-muted mb-0">2 Years Experience</p>
+                  <div>
+                    <h5 className="mb-0 font-medium text-gray-700">React</h5>
+                    <p className="text-gray-500 mb-0">2 Years Experience</p>
+                  
                   </div>
-                </div>
-              </div>
+               
             </div>
 
-            <div className="col mb-3">
-              <div className="container">
-                <div className="row skill-item rounded p-2 p-md-3 shadow-sm d-flex align-items-center">
-                  <div className="col-3 thumb-md d-flex flex-wrap justify-content-center align-items-center">
+            <div className="p-2 md:p-3 shadow-sm flex items-center space-x-3  bg-white">
+              <div className="flex justify-center items-center">
                     <img
                       src={mysqlLogo}
                       alt=""
-                      className="logo img-fluid"
+                      className="w-16 h-16"
                     />
                   </div>
-                  <div className="col-9">
-                    <h5 className="mb-0 fw-medium text-gray-700">MySQL</h5>
-                    <p className="exp text-muted mb-0">1 Year Experience</p>
-                  </div>
-                </div>
+                  <div>
+                    <h5 className="mb-0 font-medium text-gray-700">MySQL</h5>
+                    <p className="text-gray-500 mb-0">2 Years Experience</p>
+                 
               </div>
             </div>
+    
+   
+
+
           </div>
         </div>
       </section>
