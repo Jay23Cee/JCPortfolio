6aaf1a0f4311ed24c0fc317344b46e88c59a4a8c
JAYCEE <Jayceexxiii@gmail.com>
2023-05-20 00:58:07 -0700

    adding nav to open and close

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,35 +1,89 @@
-import React from 'react'
-import "../index.css"
-function Nav() {
-  return (
-    <div>
+import React, { useState } from 'react';
+import '../App.css';
 
-<div className="dl-fixed-sidebar dl-sidebar-left">
-		<div className="dl-header-container">
-			<div className="logo">
-				<h1 className="d-none d-lg-block lh-1 mb-0 mt-0">
-					<a className="lh-1" href="index.html">JACKSON </a>
-				</h1>
-				<p className="d-none d-lg-block small mb-0">FULL STACK DEVELOPER </p>
-			</div>
-			<div className="dl-burger-menu">
-				<div className="dl-line-menu dl-line-half dl-first-line"></div>
-				<div className="dl-line-menu"></div>
-				<div className="dl-line-menu dl-line-half dl-last-line"></div>
-			</div>
-			<nav className="dl-menu-fixed">
-				<ul className="list-unstyled">
-					<li><a className="pl-3" href="#home" data-scroll-nav="0">Home</a></li>
-					<li><a className="pl-3" href="#skills" data-scroll-nav="5">Skills</a></li>
-				
-					<li><a className="pl-3" href="#portfolios" data-scroll-nav="4">Portfolios</a></li>
-					<li><a className="pl-3" href="#contact" data-scroll-nav="6">Contact</a></li>
-				</ul>
-			</nav>
-		</div>
-	</div>
+function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOptionClick = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="sticky top-0 z-50 bg-gray-800">
+      <div className="lg:hidden flex justify-between items-center px-4 py-3 bg-gray-800">
+        <h1 className="text-2xl font-semibold uppercase tracking-wide text-white">
+          <a href="index.html">Jackson</a>
+        </h1>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="relative z-50"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6 text-white"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`fixed top-0 left-0 lg:relative bg-gray-800 w-full lg:w-64 z-20 transform ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        } lg:translate-x-0 transition-transform duration-200 ease-in-out`}
+      >
+        <div
+          className={`flex flex-col lg:flex-row justify-between items-start py-6 px-8 ${
+            isOpen ? 'h-screen' : 'h-auto'
+          }`}
+        >
+          <h1 className="lg:text-2xl font-semibold uppercase tracking-wide text-white">
+            <a href="index.html">Jackson</a>
+          </h1>
+          <p className="mt-2 mb-4 text-sm text-white lg:hidden">
+            Full Stack Developer
+          </p>
+          <nav className="lg:mt-0 px-8 flex flex-col lg:flex-row">
+            <a
+              className="py-2 px-4 rounded transition duration-200 hover:bg-gray-700 text-white mb-2 lg:mb-0 lg:mr-2"
+              href="#home"
+              onClick={handleOptionClick}
+            >
+              Home
+            </a>
+            <a
+              className="py-2 px-4 rounded transition duration-200 hover:bg-gray-700 text-white mb-2 lg:mb-0 lg:mr-2"
+              href="#skills"
+              onClick={handleOptionClick}
+            >
+              Skills
+            </a>
+            <a
+              className="py-2 px-4 rounded transition duration-200 hover:bg-gray-700 text-white mb-2 lg:mb-0 lg:mr-2"
+              href="#portfolios"
+              onClick={handleOptionClick}
+            >
+              Portfolios
+            </a>
+            <a
+              className="py-2 px-4 rounded transition duration-200 hover:bg-gray-700 text-white"
+              href="#contact"
+              onClick={handleOptionClick}
+            >
+              Contact
+            </a>
+          </nav>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
--- src/component/Projects.tsx
+++ src/component/Projects.tsx
@@ -3,140 +3,31 @@ import port2 from '../img/port-2.jpg';
 import workers2 from '../img/wworkers2.png';
 import byteIt from '../img/byte.jpg'
 import nikon from '../img/Nkn.png';
+import '../index.css'
+import Card from './Card'
 
 
 function projects() {
   return (
     <div>
 
-<section id="portfolios" className="portfolio-section section-padding bd-bottom px-4 px-md-5" data-scroll-index="4">
+<section id="portfolios " className="portfolio-section section-padding bd-bottom px-4 px-md-5" data-scroll-index="4">
 			<div className="container">
-				<div className="section-heading text-center mb-40 wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1000ms">
+				<div className="section-heading text-center mb-20 wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1000ms">
 					<h2>Work Portfolios</h2>
 				</div>
 
 
-				<div className="portfolio-wrapper">
-
-					<div className="row portfolio-items">
-
-					<div className="col-md-4 col-sm-6 padding-10 single-item website website">
-							<div className="portfolio-box">
-								<img src={port2} alt="img"/>
-								<div className="portfolio-content">
-									<h3 className="text-center">
-										<a href="#" type="button" className="img-popup mt-4" data-toggle="modal" data-target="#exampleModal"
-											data-id="1">
-										Library Xpress
-
-											<span></span>
-										</a>
-
-										<div className="btns">
-											<button type="button" className="btn default-btn btn-sm lh-1 p-2 mt-3" data-toggle="modal"
-												data-target="#exampleModal" data-id="1">
-												View Detail
-											</button>
-											<br />
-											<a href="https://librarycenter-production.up.railway.app/" target="_blank"
-												className="btn default-btn btn-sm lh-1 p-2 mt-3">
-												Preview
-											</a>
-										</div>
-									</h3>
-								</div>
-							</div>
-						</div> 
-
-						<div className="col-md-4 col-sm-6 padding-10 single-item website website">
-							<div className="portfolio-box">
-								<img src={workers2} alt="img"/>
-								<div className="portfolio-content">
-									<h3 className="text-center">
-										<a href="#" type="button" className="img-popup mt-4" data-toggle="modal" data-target="#exampleModal"
-											data-id="2">
-											Willing Workers
-
-											<span></span>
-										</a>
-
-										<div className="btns">
-											<button type="button" className="btn default-btn btn-sm lh-1 p-2 mt-3" data-toggle="modal"
-												data-target="#exampleModal" data-id="2">
-												View Detail
-											</button>
-											<br />
-											<a href="https://singular-biscotti-08deb7.netlify.app/" target="_blank"
-												className="btn default-btn btn-sm lh-1 p-2 mt-3">
-												Preview
-											</a>
-										</div>
-									</h3>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-4 col-sm-6 padding-10 single-item website website">
-							<div className="portfolio-box">
-								<img src={byteIt} alt="img"/>
-								<div className="portfolio-content">
-									<h3 className="text-center">
-										<a href="#" type="button" className="img-popup mt-4" data-toggle="modal" data-target="#exampleModal"
-											data-id="3">
-										Byte it
-
-											<span></span>
-										</a>
-
-										<div className="btns">
-											<button type="button" className="btn default-btn btn-sm lh-1 p-2 mt-3" data-toggle="modal"
-												data-target="#exampleModal" data-id="3">
-												View Detail
-											</button>
-											<br />
-											<a href="https://next13-experiment-beta.vercel.app/" target="_blank"
-												className="btn default-btn btn-sm lh-1 p-2 mt-3">
-												Preview
-											</a>
-										</div>
-									</h3>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-md-4 col-sm-6 padding-10 single-item website website">
-							<div className="portfolio-box">
-								<img src={nikon} alt="img"/>
-								<div className="portfolio-content">
-									<h3 className="text-center">
-										<a href="#" type="button" className="img-popup mt-" data-toggle="modal" data-target="#exampleModal"
-											data-id="4">
-											Nikon D850
-
-											<span></span>
-										</a>
-
-										<div className="btns">
-											<button type="button" className="btn default-btn btn-sm lh-1 p-2 mt-3" data-toggle="modal"
-												data-target="#exampleModal" data-id="4">
-												View Detail
-											</button>
-											<br />
-											<a href="https://vigilant-agnesi-39b86d.netlify.app/" target="_blank"
-												className="btn default-btn btn-sm lh-1 p-2 mt-3">
-												Preview
-											</a>
-										</div>
-									</h3>
-								</div>
-							</div>
-						</div> 
-					</div>
-
-
-
-
-					
-				</div>
+				<div className="@container">
+  <div className="row portfolio-items">
+    <main className="grid xl:grid-cols-2 justify-center content-center gap-5">
+      <Card img={port2} title={"Library Xpress"} />
+      <Card img={port2} title={"Library Xpress"} />
+      <Card img={port2} title={"Library Xpress"} />
+      <Card img={port2} title={"Library Xpress"} />
+    </main>
+  </div>
+</div>
 			</div>
 		</section>
 
